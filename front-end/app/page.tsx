bcbc1d5219bf8819650231b82608ac8a04eea010
TayTTo <doduyanh9803@gmail.com>
2025-07-25 16:28:06 +0700

    setup font and assets

---
+++ front-end/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello, start Next.js here (This is inter font)</h1>
+      <h1 className="font-space-grotesk">Hello, start Next.js here (This is inter font)</h1>
+      <h1>Brur, lmao</h1>
+      <h1 className="font-space-grotesk">This is Gros_tesk font</h1>
+    </>
+  );
+}
