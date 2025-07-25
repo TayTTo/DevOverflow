fc576b7ed10394a62def065bddc1c2fe283edf48
TayTTo <doduyanh9803@gmail.com>
2025-07-26 01:14:13 +0700

    back to the merge that solve problem

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
       <h1 className="font-space-grotesk">Hello, start Next.js here (This is inter font)</h1>
       <h1>Brur, lmao</h1>
       <h1 className="font-space-grotesk">This is Gros_tesk font</h1>
-      <Image src="/images/auth-dark.png" alt="Hi" width={20} height={20}/>
     </>
   );
 }
